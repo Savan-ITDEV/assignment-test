68b3ca20282fc3a0297594d8ccb5085fb452bfdb
SavanitDev <savanitdev@gmail.com>
2021-06-20 11:52:24 +0700

    init

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+// import { registerRootComponent } from "expo";
+// import React from "react";
+// import { View } from "react-native";
+// import App  from './App'
 
-import App from './App';
+// export const COUNTER_CHANGE = 'COUNTER_CHANGE'
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+// registerRootComponent(App);
+
+
+
+import { registerRootComponent } from "expo";
+import React from "react";
+
+import App  from './App'
+import { Provider } from 'react-redux';
+import configureStore from './configureStore';
+const store = configureStore()
+
+
+const RNRedux = () => (
+  <Provider store = { store }>
+    <App />
+  </Provider>
+
+registerRootComponent(RNRedux);
